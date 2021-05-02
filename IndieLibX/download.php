e12a978c1f16c1afe8861568f9ea6e36e8b15204
Tommi Rönty <tommi.ronty@iki.fi>
2021-05-02 07:04:37 +0300

    XXX

--- IndieLibX/download.php
+++ IndieLibX/download.php
@@ -14,122 +14,124 @@
 
 $filename = './README.html';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD><P></P></TD><TD><P></P></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD><P></P></TD><TD><P></P></TD></TR>";
 }
 
 $filename = './install_.sh';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD><P></P></TD><TD><P></P></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD><P></P></TD><TD><P></P></TD></TR>";
 }
 
 $filename = './IndieLibX.zip';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD><P>3D models loaded with assimp-5.0.1 static library. Build depends on SDL2-2.0.10. Some color related issues fixed.</P></TD><TD><P></P></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD><P>3D models loaded with assimp-5.0.1 static library. Build depends on SDL2-2.0.10.</P></TD><TD><P></P></TD></TR>";
 }
 
 $filename = './IndieLib_resources.zip';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD></TD><TD><P></P></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD></TD><TD><P></P></TD></TR>";
 }
 
 $filename = './IndieLibX_AllLibs_LNX32.zip';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD>Compiles. Rendering (GLSL1_1: OpenGL 3.1 / GLSL 1.40) succeeds.</TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD>Compiles. Rendering (GLSL1_1: OpenGL 3.1 / GLSL 1.40) fails. Some textures colored incorrectly as white or black.</TD><TD></TD></TR>";
 }
 
 $filename = './IndieLibX_AllLibs_LNX64.zip';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD>Compiles. Rendering (GLSL1_1: OpenGL 3.1 / GLSL 1.40) succeeds.</TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD>Compiles. Rendering (GLSL1_1: OpenGL 4.30 / GLSL 4.3) fails (DDS skybox textures, stx_SkyBox struct and testspine2 PNG textures).</TD><TD></TD></TR>";
 }
 
 $filename = './IndieLibX_AllLibs_NDK.zip';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD>Compiles. Rendering (GLES2) not tested.</TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD>Compiles. Rendering (GLES2) not tested.</TD><TD></TD></TR>";
 }
 
 $filename = './IndieLibX_AllLibs_OSX.zip';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD>Compiles. Rendering (GLSL1_1: OpenGL 2.1 / GLSL 1.2) succeeds.</TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD>Compiles. Rendering (GLSL1_1: OpenGL 2.1 / GLSL 1.2) succeeds.</TD><TD></TD></TR>";
 }
 
 $filename = './IndieLibX_AllLibs_iOS.zip';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD>Compiles. Rendering (GLES2) not tested.</TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD>Compiles. Rendering (GLES2) not tested.</TD><TD></TD></TR>";
 }
 
 $filename = './__IndieLibX_AllLibs_WIN64__.zip';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD>Compiles. Rendering (OpenGL 4.6.0 / GLSL 4.60) partially succeeds.</TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD>Compiles. Rendering (OpenGL 4.6.0 / GLSL 4.60) partially succeeds.</TD><TD></TD></TR>";
 }
 
 $filename = './IndieLibX_AllLibs_WIN64.zip';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD>Compiles. Rendering (D3D11: DirectX11) succeeds. Virtual function RendererHLSLCg::drawText disabled.</TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD>Compiles. Rendering (D3D11: DirectX11) succeeds. Virtual function RendererHLSLCg::drawText disabled.</TD><TD></TD></TR>";
 }
 
 $filename = './HLSL330toGLSL4.py';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
 }
 
 $filename = './native-activity.zip';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
 }
 
 $filename = './Lesson01.zip';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
 }
 
 $filename = './conv3ds.txt';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
 }
 
 $filename = './conv3ds.exe';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
 }
 
 $filename = './screenshots/README.html';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
 }
 
 $filename = './screenshots/NOTES1.html';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
 }
 $filename = './screenshots/NOTES2.html';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
 }
 $filename = './screenshots/NOTES3.html';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
 }
 $filename = './screenshots/NOTES4.html';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
 }
 
 $filename = './NOTES.txt';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
 }
 $filename = './Projects.xml';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
 }
 
 $filename = './download.sh';
 if (file_exists($filename)) {
-    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s.", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
+    echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/".$filename."'>".$filename."</A></TD><TD>".filesize($filename)."</TD><TD>".date("F d Y H:i:s", filemtime($filename))."</TD><TD></TD><TD></TD></TR>";
 }
 
 echo "</table>";
 echo "<table>";
+
+echo "<TR><TD><A TARGET='_blank' HREF='Nonaxisymmetric_Magnetic_Structures_in_RS_Canum_Venaticorum--type_Stars.pdf'>Nonaxisymmetric_Magnetic_Structures_in_RS_Canum_Venaticorum--type_Stars.pdf</A></TD></TR>";
 
 echo "<TR><TD><A TARGET='_blank' HREF='http://tommironty.fi/Procedural modeling of buildings/Procedural World: Procedural Architecture.html'>Procedural World: Procedural Architecture.html</A></TD></TR>";
 
