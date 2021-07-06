b76c7273694a45e96d34ea678b4706760202e058
Tommi Rönty <tommi.ronty@iki.fi>
2021-07-06 20:36:35 +0300

    X

--- IndieLibX/Libraries/HLSLCrossCompiler/tests/cs5/Issue34.hlsl
+++
@@ -1,5 +0,0 @@
-[numthreads(1, 1, 1)]
-void CSMain (uint id : SV_DispatchThreadID, RWStructuredBuffer<int> distance)
-{
-InterlockedMin (distance[2], 6);
-}
--- IndieLibX/Samples/new/new_/R2VB_VTF/gedit_.sh
+++
@@ -1,2 +0,0 @@
-gedit ./R2VB-Sort/App.cpp ./R2VB-ShadowVolume/App.cpp ./R2VB-Collision/App.cpp ./R2VB-Water/App.cpp&
-
