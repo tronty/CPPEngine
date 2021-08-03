7b1574a16d6cba921167f474f3729ddf3c7f66bb
Tommi Rönty <tommi.ronty@iki.fi>
2021-08-03 04:16:41 +0300

    X

--- IndieLibX/gedit.sh
+++
@@ -1,1 +0,0 @@
-gedit DesktopProjects/download.php clean.bat download.php Make.bat NOTES.txt screenshots/NOTES1.html screenshots/README.html screenshots/install.html &
--- IndieLib_resources/zerosizefiles.sh
+++
@@ -1,5 +0,0 @@
-./new/Island11/rock_bump4.dds
-./new/Island11/rock_bump6.dds
-./new/Island11/lichen1_normal.dds
-./new/Island11/sky.dds
-./new/Island11/terrain_rock4.dds
