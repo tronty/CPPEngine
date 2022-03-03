51ff4d91689af078c3859a7182a2ed265abc8633
Tommi Rönty <tronty@dcx-d8yyyyyyyyyyyyycy-3.bb.dnainternet.fi>
2022-03-03 16:57:15 +0200

    X

---
+++ IndieLibX/Libraries/compare.sh
@@ -0,0 +1,8 @@
+diff ./Framework3_GLSL/src/RendererGL_1_1.cpp ./Framework3/src/RendererGL_1_1.cpp &&
+diff ./Framework3_GLSL/src/RendererHLSLCg.cpp ./Framework3/src/RendererHLSLCg.cpp &&
+diff ./Framework3_GLSL/src/RendererGLSLGL_1_1.cpp ./Framework3/src/RendererGLSLGL_1_1.cpp &&
+diff ./Framework3_GLSL/include/Framework3/RendererGLSLGL_1_1.h ./Framework3/include/Framework3/RendererGLSLGL_1_1.h &&
+diff ./Framework3_GLSL/include/Framework3/RendererHLSLCg.h ./Framework3/include/Framework3/RendererHLSLCg.h &&
+diff ./Framework3_GLSL/include/Framework3/IRenderer.h ./Framework3/include/Framework3/IRenderer.h &&
+diff ./Framework3_GLSL/include/Framework3/RendererGL_1_1.h ./Framework3/include/Framework3/RendererGL_1_1.h
+
