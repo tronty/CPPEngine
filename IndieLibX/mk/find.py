8eed417adcee2b2cfc5929c39e5f89597f63dd22
Tommi Rönty <tronty@dcx-d8yyyyyyyyyyyyycy-3.bb.dnainternet.fi>
2022-04-05 21:47:56 +0300

    x

--- IndieLibX/mk/find.py
+++ IndieLibX/mk/find.py
@@ -1,13 +1,30 @@
 import os, sys, re
 
-def find(filePath):
+def replaceString(filePath):
     input = open(filePath)
     text=input.read()
     input.close()
-    v=re.findall(r"\bnoise\b", text)
-    if len(v):
-        print filePath+':'
-    return
+
+    text=re.sub(r'\btexture\b', 'texture2D', text)
+    text=re.sub(r'\btexelFetch\b', 'texture2D', text)
+    text=re.sub(r'\btextureLod\b', 'texture2D', text)
+
+    return text
+
+def replaceStringInFile(filePath):
+    "replaces all string by a regex substitution"
+    #print filePath
+    tempName=filePath+'~~~'
+    input = open(filePath)
+    outtext=input.read()
+    input.close()
+    outtext=replaceString(outtext)
+
+    output = open(tempName,'w')
+    output.write(outtext)
+    output.close()
+    os.unlink(filePath)
+    os.rename(tempName,filePath)
 	
 def myfun(dummy, dirr, filess):
 	#print dummy, dirr, filess
@@ -15,9 +32,12 @@ def myfun(dummy, dirr, filess):
 	    #print child
 	    
 	    if '.shd' == os.path.splitext(child)[1] and os.path.isfile(dirr+'/'+child):
-	    	    find(dirr+'/'+child)
+	    	    replaceStringInFile(dirr+'/'+child)
+	    
+	    if '.glsl' == os.path.splitext(child)[1] and os.path.isfile(dirr+'/'+child):
+	    	    replaceStringInFile(dirr+'/'+child)
 
-v=['../../IndieLib_resources/www.shadertoy.com']
+v=['../../IndieLib_resources/shd/']
 
 for i in v:
 	os.path.walk(i, myfun, 13)
