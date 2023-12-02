04e5d809dac3727b72b5237e2ed5926285defab0
Tommi Rönty <tronty@dcx-d8yyyyyyyyyyyyycy-3.bb.dnainternet.fi>
2023-12-02 10:55:01 +0200

    X

---
+++ IndieLibX/Samples/TestNoise/TestGeometryShader.cpp
@@ -0,0 +1,117 @@
+/*
+  Copyright (c) 2023 Tommi Roenty   http://www.tommironty.fi/
+  Licensed under The GNU Lesser General Public License, version 2.1:
+      http://opensource.org/licenses/LGPL-2.1
+*/
+#include <Framework3/IRenderer.h>
+
+ShaderID shd=-1;
+VertexFormatID vf=-1;
+float mAngleX=0.0f;
+float mAngleY=0.0f;
+
+int init(const char* aTitle)
+{
+	shd=-1;
+	vf=-1;
+
+		ShaderID id=IRenderer::GetRendererInstance()->addShaderFromFile("/shd/GeometryShader.glsl", "main", "main", "main");
+		if(id!=-1)
+			shd=id;
+		#if 1
+	FormatDesc format[] =
+	{
+		0, TYPE_VERTEX,   FORMAT_FLOAT, 2,
+		0, TYPE_TEXCOORD, FORMAT_FLOAT, 2
+	};
+		#else
+		FormatDesc format[] =
+		{
+			0, TYPE_VERTEX,   FORMAT_FLOAT, 3,
+			0, TYPE_NORMAL,   FORMAT_FLOAT, 3,
+			0, TYPE_BINORMAL, FORMAT_FLOAT, 3,
+			0, TYPE_TANGENT,  FORMAT_FLOAT, 3,
+			0, TYPE_TEXCOORD, FORMAT_FLOAT, 3,
+			0, TYPE_TEXCOORD, FORMAT_FLOAT, 2
+		};
+		#endif
+		if(shd>-1)
+			vf=IRenderer::GetRendererInstance()->addVertexFormat(format, elementsOf(format), shd);
+	return 0;
+}
+
+void render()
+{
+	float f=128.0f/256.0f;
+	IRenderer::GetRendererInstance()->Clear(true, true, D3DXFROMWINEVECTOR4 (f, f, f, 1.0f));
+	IRenderer::GetRendererInstance()->BeginScene();
+
+		D3DXFROMWINEMATRIX W,R,S;
+		D3DXFROMWINEMatrixRotationYawPitchRoll( &R,
+		                            D3DXFROMWINEToRadian(mAngleX),
+		                            D3DXFROMWINEToRadian(mAngleY),
+		                            0.0f );
+
+
+		D3DXFROMWINEVECTOR4 silver=stx_GetRGBA(eSilver);
+		D3DXFROMWINEVECTOR4 AmbientColor=silver;
+		D3DXFROMWINEVECTOR4 DiffuseColor=0.1f*silver;
+		D3DXFROMWINEVECTOR4 LightDir(0.0f, 0.0f, -1.0f, 1.0f);
+		D3DXFROMWINEVECTOR4 EyeDir(0.0f, 0.0f, 1.0f, 1.0f);
+
+	//D3DXFROMWINEMatrixIdentity(&R);
+	float s=0.25f*4.0f;
+	D3DXFROMWINEMatrixScaling(&S, s, s, s);
+	W=R*S;
+
+	D3DXFROMWINEMATRIX I;
+	D3DXFROMWINEMatrixIdentity(&I);
+	if(shd==-1) return;
+	if(vf==-1) return;
+#if 1
+	IRenderer::GetRendererInstance()->setShader(shd);
+	IRenderer::GetRendererInstance()->setVertexFormat(vf);
+	D3DXFROMWINEVECTOR4 color(0.6f, 0.6f, 0.6f, 1.0f);
+	IRenderer::GetRendererInstance()->setShaderConstant4f("color", color);
+	
+	IRenderer::GetRendererInstance()->setShaderConstant4x4f("worldViewProj", I);
+	IRenderer::GetRendererInstance()->setDepthState(IRenderer::GetRendererInstance()->GetnoDepthTest());
+	#if 1
+	//		           x	 y     tx    ty
+	float v[16] =  {  1.0f,-1.0f, 1.0f, 1.0f,
+			          1.0f, 1.0f, 1.0f, 0.0f,
+			         -1.0f,-1.0f, 0.0f, 1.0f,
+			         -1.0f, 1.0f, 0.0f, 0.0f };
+	unsigned int N=4;
+	#else
+	float v[16] =  {  1.0f,-1.0f,
+			           1.0f, 1.0f,
+			          -1.0f,-1.0f,
+			          -1.0f, 1.0f };
+	unsigned int N=2;
+	#endif
+#if 0
+	IRenderer::GetRendererInstance()->DrawPrimitiveUP(PRIM_TRIANGLE_STRIP, 2, &v[0], &v[0], N*sizeof(float));
+#else
+	__WORD__ i[6] ={0,1,2,2,1,3};
+	IRenderer::GetRendererInstance()->DrawIndexedPrimitiveUP(PRIM_TRIANGLES, 0, 4, 2, &i[0], &i[0], CONSTANT_INDEX2, &v[0], &v[0], N*sizeof(float));
+#endif
+#endif
+
+	IRenderer::GetRendererInstance()->EndScene();
+	IRenderer::GetRendererInstance()->Present( );
+}
+
+int ApplicationLogic()
+{
+	IRenderer* r=IRenderer::GetRendererInstance("TestGeometryShader");	
+	IInput*    i=STX_Service::GetInputInstance();
+	init("");
+	while (!i->OnKeyPress (STX_KEY_ESCAPE) && !i->Quit())
+	{
+		i->Update();
+		render();
+	}
+	return 0;
+}
+
